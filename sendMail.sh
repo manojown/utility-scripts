8e1a16df79ca1330793a8ab1a6e618c212044efe
Manoj Choudhary <manojown1@gmail.com>
2018-08-30 14:33:48 +0530

    check docker size if not match criteria send mail

---
+++ sendMail.sh
@@ -0,0 +1,25 @@
+REMAINING=$(df -h | grep "/var/lib/docker/overlay2/*"  | head -1 | awk '{print $4}' | rev | cut -c 2- | rev)
+USED=$(df -h | grep "/var/lib/docker/overlay2/*"  | head -1 | awk '{print $3}')
+echo  ${REMAINING}
+LIMIT=21
+if [[ $REMAINING -gt $LIMIT ]]
+then
+ echo "Enough sapce we have dont worry"
+else
+       read -d '' environment <<"EOF"
+To:<TOMAIL>
+From:<FROMMAIL>
+Subject: Critical Alert
+
+Your server uses reach <USED> and Available space is only  - <REMAINING>. ALERT (CRITICAL)
+
+EOF
+
+ cd /etc/ssmtp/
+ rm mail.txt
+ touch mail.txt
+ echo "$environment" > ./mail.txt
+ sed -i "s/<REMAINING>/${REMAINING}GB/g" mail.txt
+ sed -i "s/<USED>/${USED}/g" mail.txt
+ df -h >> mail.txt | sendmail -t < mail.txt
+fi
